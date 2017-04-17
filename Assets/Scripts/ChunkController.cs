6eb518c1cf2ea001c23debcf451fb81b43787272
Travis <pixeleater11@gmail.com>
2017-04-17 19:11:02 +1000

    Changed things

---
+++ Assets/Scripts/ChunkController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkController : MonoBehaviour
+{
+    [SerializeField]
+    int chunkSize;
+    [SerializeField]
+    Ground groundObject; 
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        GenerateMap();
+	}
+	
+    /// <summary>
+    /// Generates the playing field
+    /// It takes the ChunkSize given and generates the map in both the x and z direction. e.g. 10 chunkSize = 100 points
+    /// </summary>
+    void GenerateMap()
+    {
+        Vector3 spawnPosition;
+        for (int x = 0; x <= chunkSize; x++)
+        {
+            for(int y = 0; y <= chunkSize; y++)
+            {
+                Instantiate(groundObject, spawnPosition = new Vector3(x, 0, y), transform.rotation);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+   // public GameObject groundObject;
+
+    [SerializeField]
+    int oilValue;
+
+    [SerializeField]
+    bool builtOn; 
+  
+	// Use this for initialization
+	void Start ()
+    {
+        builtOn = false; 
+        oilValue = Mathf.FloorToInt(Random.Range(0, 100)); 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
